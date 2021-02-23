a573e4936f3f49a0de37295157baa7553a9fdaa9
namjiwon1023 <nzy1414117007@gmail.com>
2021-02-23 19:48:26 +0900

    Organize_the_warehouse

--- channel_dqn/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Update the environment, then run the agent
-cp ChannelEnv.py /home/nam/gym/gym/envs/classic_control/ChannelEnv.py
-cp channelConfig.py /home/nam/gym/gym/envs/classic_control/channelConfig.py
-python ./channel1.py
-
