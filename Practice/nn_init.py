29cd1552dcf846c656d7d91a5a03a4a80c5df3cb
namjiwon1023 <nzy1414117007@gmail.com>
2021-03-02 16:21:58 +0900

    update_nn_init

---
+++ Practice/nn_init.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+
+
+def _layer_norm(layer, std=1.0, bias_const=1e-6):
+    torch.nn.init.orthogonal_(layer.weight, std)
+    torch.nn.init.constant_(layer.bias, bias_const)
