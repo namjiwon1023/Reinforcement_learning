c9b597096772f5bdfd0bab2ac2913ee7dab69e4a
namjiwon1023 <nzy1414117007@gmail.com>
2021-03-11 19:38:15 +0900

    update_TD3

--- TD3/ActorNetwork.py
+++ TD3/ActorNetwork.py
@@ -8,7 +8,7 @@ class ActorNetwork(nn.Module):
         checkpoint_path='/home/nam/Reinforcement_learning/TD3'):
         super(ActorNetwork, self).__init__()
         self.checkpoint_path = checkpoint_path
-        self.dirPath = os.path.join(self.checkpoint_path, 'torch_TD3_actor')
+        self.dirPath = os.path.join(self.checkpoint_path, 'torch_TD3_actor_2')
         self.max_action = max_action
         self.actor = nn.Sequential(nn.Linear(n_states, 256),
                                     nn.ReLU(),
@@ -28,8 +28,8 @@ class ActorNetwork(nn.Module):
 
     def save_models(self):
         T.save(self.state_dict(), self.dirPath)
-        T.save(self.optimizer.state_dict(), self.dirPath + '_optimizer')
+        T.save(self.optimizer.state_dict(), self.dirPath + '_optimizer_2')
 
     def load_models(self):
         self.load_state_dict(T.load(self.dirPath))
-        self.optimizer.load_state_dict(T.load(self.dirPath + '_optimizer'))
+        self.optimizer.load_state_dict(T.load(self.dirPath + '_optimizer_2'))
