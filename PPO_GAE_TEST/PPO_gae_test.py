ebca9fee26cb724d5a8e003f90596882bdfd4f5b
namjiwon1023 <nzy14141117007@gmail.com>
2021-03-21 23:20:37 +0900

    update_gae_ppo

---
+++ PPO_GAE_TEST/PPO_gae_test.py
@@ -0,0 +1,140 @@
+import math
+import random
+import numpy as np
+import gym
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Normal
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+env_name = 'Pendulum-v0'
+env = gym.make(env_name)
+
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        nn.init.normal_(m.weight, mean=0., std=0.1)
+        nn.init.constant_(m.bias, 0.1)
+
+class ActorCritic(nn.Module):
+    def __init__(self, num_inputs, num_outputs, hidden_size, std=0.0):
+        super(ActorCritic, self).__init__()
+        self.critic = nn.Sequential(nn.Linear(num_inputs, hidden_size), nn_ReLU(),
+                                    nn.Linear(hidden_size, 1))
+        self.actor = nn.Sequential(nn.Linear(num_inputs, hidden_size), nn.ReLU(),
+                                    nn.Linear(hidden_size, num_outputs))
+        self.log_std = nn.Parametsr(torch.ones(1, num_outputs) * std)
+
+        self.apply(init_weights)
+    def forward(self, x):
+        value = self.critic(x)
+        mu = self.actor(x)
+        std = self.log_std.exp().expand_as(mu)
+        dist = Normal(mu, std)
+        return dist, value
+
+def compute_gae(next_value, rewards, masks, values, gamma=0.99, tau=0.95):
+    values = values + [next_value]
+    gae = 0
+    returns = []
+    for step in reversed(range(len(rewards))):
+        delta = rewards[step] + gamma * values[step + 1] * masks[step] - values[step]
+        gae = delta + gamma * tau * masks[step] * gae
+        returns.insert(0, gae + values[step])
+    return returns
+
+def ppo_iter(mini_batch_size, states, actions, log_probs, returns, advantage):
+    batch_size = states.size(0)
+    for _ in range(batch_size // mini_batch_size):
+        rand_ids = np.random.randint(0, batch_size, mini_batch_size)
+        yield states[rand_ids, :], actions[rand_ids, :], log_probs[rand_ids, :], returns[rand_ids, :], advantage[rand_ids, :]
+
+def ppo_update(ppo_epochs, mini_batch_size, states, actions, log_probs, returns, advantages, clip_param=0.2):
+    for _ in range(ppo_epochs):
+        for state, action, old_log_probs, return_, advantage in ppo_iter(mini_batch_size, states, actions, log_probs, returns, advantages):
+            dist, value = model(state)
+            entropy = dist.entropy().mean()
+            new_log_probs = dist.log_prob(action)
+
+            ratio = (new_log_probs - old_log_probs).exp()
+            surr1 = ratio * advantage
+            surr2 = torch.clamp(ratio, 1.0 - clip_param, 1.0 + clip_param) * advantage
+
+            actor_loss = -torch.min(surr1, surr2).mean()
+            critic_loss = (return_ - value).pow(2).mean()
+
+            loss = 0.5 * critic_loss + actor_loss - 0.001 * entropy
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+num_inputs = env.observation_space.shape[0]
+num_outputs = env.action_space.shape[0]
+
+hidden_size = 256
+lr = 3e-4
+num_steps = 20
+mini_batch_size = 5
+ppo_epochs = 4
+threshold_reward = -200
+
+model = ActorCritic(num_inputs, num_outputs, hidden_size).to(device)
+optimizer = optim.Adam(model.parameters(), lr=lr)
+
+max_frames = 15000
+frame_idx = 0
+
+state = env.reset()
+
+while frame_idx < max_frames:
+
+    log_probs = []
+    values = []
+    states = []
+    actions = []
+    rewards = []
+    masks = []
+    entropy = 0
+
+    for _ range(num_steps):
+        state = torch.FloatTensor(state).to(device)
+        dist, value = model(state)
+
+        action = dist.sample()
+        next_state, reward, done, _ = env.step(action.cpu().numpy())
+
+        log_prob = dist.log_prob(action)
+        entropy += dist.entropy().mean()
+
+        log_probs.append(log_prob)
+        values.append(value)
+        rewards.append(torch.FloatTensor(reward).unsqueeze(1),to(device))
+        masks.append(torch.FloatTensor(1 - done).unsqueeze(1).to(device))
+
+        states.append(state)
+        actions.append(action)
+
+        state = next_state
+        frame_idx += 1
+
+    next_state = torch.FloatTensor(next_state).to(device)
+    _, next_value = model(next_state)
+    returns = compute_gae(next_value, rewards, masks, values)
+
+    returns = torch.cat(returns).detach()
+    log_probs = torch.cat(log_probs).detach()
+    values = torch.cat(values).detach()
+    states = torch.cat(states)
+    actions = torch.cat(actions)
+    advantage = returns - values
+
+    ppo_update(ppo_epochs, mini_batch_size, states, actions, log_probs, returns, advantage)
+
+
+# expert_traj = []
+# expert_traj.append(np.hstack([state, action]))
+# expert_traj = np.stack(expert_traj)
+# np.save("expert_traj.npy", expert_traj)
