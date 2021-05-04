cd27284e153cca66ae6d5dee08723c0567f7e5c1
namjiwon1023 <nzy1414117007@gmail.com>
2021-05-04 23:19:07 +0900

    update_MADDPG

--- MADDPG/utils.py
+++ MADDPG/utils.py
@@ -1,3 +1,4 @@
+# -*- coding:utf8 -*-
 import random
 import numpy as np
 import torch as T
@@ -22,3 +23,31 @@ def _random_seed(seed):
     np.random.seed(seed)
     random.seed(seed)
     print('Using GPU : ', T.cuda.is_available() , ' |  Seed : ', seed)
+
+def make_env(args):
+    from multiagent.environment import MultiAgentEnv
+    import multiagent.scenarios as scenarios
+
+    # load scenario from script
+    scenario = scenarios.load(args.scenario_name + '.py').Scenario()
+
+    # create world
+    world = scenario.make_world()
+
+    # create multiagent environment
+    env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation)
+
+    args.n_players = env.n # The number of all players including enemies
+    args.n_agents = env.n - args.num_adversaries # The number of players that need to be controlled, although the enemy can also be controlled, but if both parties learn, different algorithms are required
+    args.obs_shape = [env.observation_space[i].shape[0] for i in range(args.n_agents)] # Each dimension represents the obs dimension of the agent
+
+    action_shape = []
+    for content in env.action_space:
+        action_shape.append(content.n)
+    args.action_shape = action_shape[:args.n_agents] # Each dimension represents the act dimension of the agent
+
+    '''Define the highest and lowest value of the action'''
+    args.high_action = 1
+    args.low_action = -1
+
+    return env, args
