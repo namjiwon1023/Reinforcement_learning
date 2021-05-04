cf457303f745967532e52276c7063c94748efa50
namjiwon1023 <nzy14141117007@gmail.com>
2021-05-05 01:02:26 +0900

    update_maddpg

--- MADDPG/MADDPG.py
+++ MADDPG/MADDPG.py
@@ -6,7 +6,6 @@ from ActorCriticNetwork import Actor, Critic
 import numpy as np
 import random
 import copy
-import pickle
 
 class MADDPG:
     '''# Because the obs and act dimensions of different agents may be different, so the neural network is different, and agent_id is needed to distinguish'''
@@ -14,10 +13,11 @@ class MADDPG:
         self.args = args
         self.agent_id = agent_id
         self.train_step = 0
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
 
         # Create the Actor-Critic Network
-        self.actor_network = Actor(args, agent_id)
-        self.critic_network = Critic(args)
+        self.actor_network = Actor(args, agent_id).to(self.device)
+        self.critic_network = Critic(args).to(self.device)
 
         # build up the actor target network
         self.actor_target_network = copy.deepcopy(self.actor_network)
@@ -56,6 +56,7 @@ class MADDPG:
             print('Agent {} successfully loaded actor network: {}'.format(self.agent_id, self.model_path + '/actor_params.pkl'))
             print('Agent {} successfully loaded critic network: {}'.format(self.agent_id, self.model_path + '/critic_params.pkl'))
 
+    # soft update
     def _soft_update_target_network(self):
         with T.no_grad():
             for t_p, l_p in zip(self.actor_target_network.parameters(), self.actor_network.parameters()):
@@ -63,3 +64,63 @@ class MADDPG:
 
             for t_p, l_p in zip(self.critic_target_network.parameters(), self.critic_network.parameters()):
                 t_p.data.copy_(self.args.tau * l_p.data + (1 - self.args.tau) * t_p.data)
+
+    # update the Actor-Critic Networks
+    def learn(self, transitions, other_agents):
+        for key in transitions.keys():
+            transitions[key] = T.as_tensor(transitions[key], dtype=T.float32, device=self.device)
+        r = transitions['r_%d' % self.agent_id] # You only need your own reward during training
+        o, u, o_next = [], [], [] # Used to store the various items in each agent's experience
+        for agent_id in range(self.args.n_agents):
+            o.append(transitions['o_%d' % agent_id])
+            u.append(transitions['u_%d' % agent_id])
+            o_next.append(transitions['o_next_%d' % agent_id])
+
+        # calculate the target Q value function
+        u_next = []
+        with T.no_grad():
+            # Get the action corresponding to the next state
+            index = 0
+            for agent_id in range(self.args.n_agents):
+                if agent_id == self.agent_id:
+                    u_next.append(self.actor_target_network(o_next[agent_id]))
+                else:
+                    # Because the incoming other_agents is one less than the total number, maybe some agent in the middle is the current agent and cannot be traversed to select actions
+                    u_next.append(other_agents[index].policy.actor_target_network(o_next[agent_id]))
+                    index += 1
+
+            q_next = self.critic_target_network(o_next, u_next).detach()
+            target_q = (r.unsqueeze(1) + self.args.gamma * q_next).detach()
+
+
+        q_value = self.critic_network(o, u)
+        critic_loss = (target_q - q_value).pow(2).mean()
+
+        # actor loss
+        # Reselect the action of the current agent in the joint action, and the actions of other agents remain unchanged
+        u[self.agent_id] = self.actor_network(o[self.agent_id])
+        actor_loss = -self.critic_network(o, u).mean()
+
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        self._soft_update_target_network()
+        if self.train_step > 0 and self.train_step % self.args.save_rate == 0:
+            self.save_model(self.train_step)
+        self.train_step += 1
+
+    def save_model(self, train_step):
+        num = str(train_step // self.args.save_rate)
+        model_path = os.path.join(self.args.save_dir, self.args.scenario_name)
+        if not os.path.exists(model_path):
+            os.makedirs(model_path)
+        model_path = os.path.join(model_path, 'agent_%d' % self.agent_id)
+        if not os.path.exists(model_path):
+            os.makedirs(model_path)
+        T.save(self.actor_network.state_dict(), model_path + '/' + num + '_actor_params.pkl')
+        T.save(self.critic_network.state_dict(),  model_path + '/' + num + '_critic_params.pkl')
